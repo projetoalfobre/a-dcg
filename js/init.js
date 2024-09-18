d98d3d829f49173799d81dd5f44c4b2b3923b61d
nafergo <nafergo@gmail.com>
2024-09-18 19:07:25 +0100

    1

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
+    $('.collapsible').collapsible();
+    $('.tabs').tabs();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
